23399c1ba57b265f39c8ac3f00eec5c46a79f9f7
GoDeepak1 <119528621+GoDeepak1@users.noreply.github.com>
2024-08-09 11:18:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const axios = require('axios');
 const { Telegraf } = require('telegraf');
 
-const bot=new Telegraf('//API KEY');
+const bot=new Telegraf('//YOUR API KEY HERE');
 
 bot.start((ctx) => ctx.reply('WELCOME TO MY NEW CUSTOMIZED BOT'));
 bot.command('dsvlp1', async function(ctx){
-    const response=await axios.get('https://raw.githubusercontent.com/GoDeepak1/dsvlp/main/1a.py');
+    const response=await axios.get('//FileLinkHere');
     // console.log(response.data);
     return ctx.reply(response.data);
 
 
 });
 bot.command('dsvlp2', async function(ctx){
-    const response=await axios.get('https://raw.githubusercontent.com/GoDeepak1/dsvlp/main/2a.py');
+    const response=await axios.get('//FileLinkHere');
     // console.log(response.data);
     return ctx.reply(response.data);
 
 
 });
 bot.command('gg',async function(ctx){
-    const response=await axios.get('https://raw.githubusercontent.com/GoDeepak1/Quiz-App/main/index.html');
+    const response=await axios.get('//FileLinkHere');
     return ctx.reply(response.data)
 });
 bot.launch();
